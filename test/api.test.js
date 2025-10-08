ca96915c243230ec7d19e1bb35c872f3a48f5fbd
Cris Niel Baloloy <cnbaloloy@pcdsi.ph>
2025-10-08 10:54:53 +0800

    fix ci workflow

--- test/api.test.js
+++ test/api.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/index'); // adjust path if needed
+const app = require('../src/index'); // import app, not server
 
 describe('GET /', () => {
   it('should return Hello World!', async () => {
